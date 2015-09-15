90ed5b6dc8b5e8931dfaf871aa305f58db70f409
Michael <michaeltran_93@hotmail.com>
2015-09-16 00:01:28 +0200

    Added a Map fragment to the Main activity

--- BusExplorer/app/src/main/java/com/dat255tesla/busexplorer/MainActivity.java
+++ BusExplorer/app/src/main/java/com/dat255tesla/busexplorer/MainActivity.java
@@ -1,34 +1,73 @@
 package com.dat255tesla.busexplorer;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-    private Button helloButton;
-    private TextView helloText;
-    private EditText editName;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends FragmentActivity {
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        helloButton = (Button) findViewById(R.id.helloButton);
-        helloText = (TextView) findViewById(R.id.helloText);
-        editName = (EditText) findViewById(R.id.editName);
+        setUpMapIfNeeded();
+    }
 
-        View.OnClickListener hello = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = editName.getText().toString();
-                helloText.setText("Hello, " + name + "!");
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
             }
-        };
+        }
+    }
 
-        helloButton.setOnClickListener(hello);
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.setMyLocationEnabled(true);
+        if(mMap.getMyLocation() != null){ //Note that GoogleMap.getMyLocation() is deprecated and only used for testing
+            LatLng latlng = new LatLng(mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude());
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 18));
+        } else{
+            Toast.makeText(getApplicationContext(), "getMyLocation() gives null", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
