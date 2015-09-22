ec6fccccf5d4ed5063b80821b91b22f53151e803
Patrick Franz <patfra71@gmail.com>
2015-09-23 00:22:10 +0200

    Added listener to list-elements

--- BusExplorer/app/src/main/java/com/dat255tesla/busexplorer/MainActivity.java
+++ BusExplorer/app/src/main/java/com/dat255tesla/busexplorer/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -41,6 +43,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sites);
         ListView listView = (ListView) findViewById(R.id.listBelowMap);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        (String) parent.getItemAtPosition(position) + " clicked", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
 
         setUpMapIfNeeded();
     }
