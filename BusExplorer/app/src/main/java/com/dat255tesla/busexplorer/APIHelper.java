41963bd324440923f6df039b323992c78e84fff1
Michael <michaeltran_93@hotmail.com>
2015-09-24 14:50:50 +0200

    Parse relevant info from API call

--- BusExplorer/app/src/main/java/com/dat255tesla/busexplorer/APIHelper.java
+++ BusExplorer/app/src/main/java/com/dat255tesla/busexplorer/APIHelper.java
@@ -3,10 +3,15 @@ package com.dat255tesla.busexplorer;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Date;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -21,9 +26,9 @@ public class APIHelper extends AsyncTask<String, String, String> {
         this.tv = tv;
     }
 
-    private String doGet(String encoded) throws IOException {
+    private String doGet(String encoded) throws IOException, JSONException {
         long t2 = System.currentTimeMillis();
-        long t1 = t2 - (1000 * 120);
+        long t1 = t2 - (1000 * 60);
 
         StringBuffer response = new StringBuffer();
         //TODO Enter your base64 encoded Username:Password
@@ -48,16 +53,30 @@ public class APIHelper extends AsyncTask<String, String, String> {
 
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
+            System.out.println("inputLine printout: " + inputLine);
         }
         in.close();
         System.out.println(response.toString());
-        return response.toString();
+
+        String printout = "";//TEMP TEST
+
+        JSONArray jsonArray = new JSONArray(response.toString());
+        for(int i = 0; i < jsonArray.length(); i++){
+            JSONObject object = jsonArray.getJSONObject(i);
+            printout = printout + "\nNext stop: " + object.getString("value") + "\nTimestamp: " + new Date(object.getLong("timestamp"));
+        }
+
+        return printout;
     }
 
     @Override
     protected String doInBackground(String... params) {
         try {
-            return doGet(params[0]);
+            try {
+                return doGet(params[0]);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
