4b652fb9acfd0b77d8df670fa2a111cf1a1bc2f0
Sebastian K <fredrik.sebastian.k@gmail.com>
2015-10-12 23:39:38 +0200

    Added GPRMC parser

--- BusExplorer/app/src/main/java/com/dat255tesla/busexplorer/explorercontent/MapUtils.java
+++ BusExplorer/app/src/main/java/com/dat255tesla/busexplorer/explorercontent/MapUtils.java
@@ -65,6 +65,50 @@ public class MapUtils {
         });
     }
 
+    /**
+     * Parses an RMC string returning its latitude and longitude.
+     * What a shitty format. Seriously.
+     *
+     * @param gprmc The string to be decoded
+     * @return A {@link LatLng} containing the coordinates given in the RMC string.
+     */
+    public static LatLng ParseRMC(String gprmc) {
+        LatLng latlng;
+
+        // TODO null checks, size checks, etc
+
+        String[] parts = gprmc.split(",");
+        String lat = parts[3];
+        String lng = parts[5];
+        String ns = parts[4];
+        String ew = parts[6];
+
+        Double seconds =
+                Double.parseDouble(lat.substring(5,7) + "." + lat.substring(7));
+        Double newLat = Double.parseDouble(lat.substring(0,2)) +
+                (Double.parseDouble(lat.substring(2,4)) / 60) +
+                seconds / 3600;
+
+        seconds =
+                Double.parseDouble(lng.substring(6,8) + "." + lng.substring(8));
+        Double newLng = Double.parseDouble(lng.substring(0,3)) +
+                (Double.parseDouble(lng.substring(3,5)) / 60) +
+                seconds / 3600;
+
+        // If south of equator
+        if (ns.equals('S')) {
+            newLat *= -1;
+        }
+
+        // If west of greenwich
+        if (ew.equals('W')) {
+            newLng *= -1;
+        }
+
+        latlng = new LatLng(newLat, newLng);
+        return latlng;
+    }
+
     public static List<Marker> MarkersInRange(HashMap<Marker, InfoNode> map, Location center,
                                               int maxDist) {
 
